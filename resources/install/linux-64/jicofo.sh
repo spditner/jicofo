7f30dc59b9c17490b3290e4de6634d1a309e5bfd
Simon Ditner <spditner@gmail.com>
2015-08-22 17:45:22 -0400

    Pass java flags in from init script
    Allows setting java runtime values such as sip-communicator flags by
    setting JAVA_FLAGS in /etc/jitsi/jicofo/config, rather supplying them
    in /usr/share/jicofo/.sip-communicator/sip-commnicator.properties
    
    For example, enabling the reservation callback in
    /etc/jitsi/jicofo/config becomes:
    
       JAVA_FLAGS="-Dorg.jitsi.impl.reservation.rest.BASE_URL=http://localhost"

--- resources/install/linux-64/jicofo.sh
+++ resources/install/linux-64/jicofo.sh
@@ -22,4 +22,4 @@ cp=$(JARS=($SCRIPT_DIR/jicofo*.jar $SCRIPT_DIR/lib/*.jar); IFS=:; echo "${JARS[*
 libs="$SCRIPT_DIR/lib/native/linux-64"
 logging_config="$SCRIPT_DIR/lib/logging.properties"
 
-LD_LIBRARY_PATH=$libs java -Xmx3072m -XX:-HeapDumpOnOutOfMemoryError -Djava.library.path=$libs -Djava.util.logging.config.file=$logging_config -cp $cp $mainClass $@
+LD_LIBRARY_PATH=$libs java -Xmx3072m -XX:-HeapDumpOnOutOfMemoryError -Djava.library.path=$libs -Djava.util.logging.config.file=$logging_config $JAVA_OPTIONS -cp $cp $mainClass $@
